29d128edb457ee2e2fcf8aab0fd70ca7af482b09
Paul Serby <paul.serby@clock.co.uk>
2011-05-06 16:07:21 +0100

    Adding Maiden build script

---
+++ Maiden.php
@@ -0,0 +1,34 @@
+<?php
+namespace Maiden;
+
+/**
+ * Build for the Maiden tool itself. This is only really used to install and uninstall.
+ */
+class MaidenProject extends MaidenDefault {
+
+	/**
+	 * Looks for messy code using PHPMD
+	 */
+	public function checkForMess() {
+		$this->exec("phpmd lib text codesize,unusedcode,naming,design");
+	}
+
+	/**
+	 * Runs all the phpunit tests
+	 */
+	public function test() {
+		$this->clean();
+		chdir("test");
+		$this->exec("phpunit --log-junit ../build/log/phpunit.xml --coverage-clover ../build/coverage/coverage.xml --bootstrap PitonBootstrap.php Piton");
+	}
+
+	/**
+	 * Runs all the phpunit tests
+	 */
+	public function clean() {
+		$this->exec("rm -rf build");
+		mkdir("build/coverage", 0775, true);
+		mkdir("build/log", 0775, true);
+	}
+
+}
